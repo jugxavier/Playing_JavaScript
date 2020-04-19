5b66e02afba9e660742905f2a926d3d7f5d7ab4e
João Henrique Xavier <jhcxavier@gmail.com>
2020-04-19 00:16:15 +0000

    update

--- Exercise/replits.js
+++ Exercise/replits.js
@@ -188,3 +188,16 @@ function getFullName(firstName, lastName) {
 
 }
 console.log(getFullName('Joe', 'Smith'))
+
+//------------------------------------------------
+// Write a function called "getLengthOfWord".
+
+// Given a word, "getLengthOfWord" returns the length of the given word.
+
+// var output = getLengthOfWord('some');
+// console.log(output); // --> 4
+function getLengthOfWord(word) {
+//your code here
+
+    }
+console.log(getLengthOfWord('some'))
